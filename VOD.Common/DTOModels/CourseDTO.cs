00120d94ecebeb735b10ba1b3762c55fd8fc9e5c
Lavar.Design <bjorn.osterholm@elnotorios.com>
2020-01-04 23:14:25 +0100

    Membership Controller and Automapper done

--- VOD.Common/DTOModels/CourseDTO.cs
+++ VOD.Common/DTOModels/CourseDTO.cs
@@ -9,7 +9,8 @@ namespace VOD.Common.DTOModels
         public int CourseId { get; set; }
         public string CourseTitle { get; set; }
         public string CourseDescription { get; set; }
-        public string CourseImageUrl { get; set; }
         public string MarqueeImageUrl { get; set; }
+        public string CourseImageUrl { get; set; }
+
     }
 }
