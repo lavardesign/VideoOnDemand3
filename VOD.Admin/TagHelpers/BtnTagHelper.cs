b40d45bf4fa79be1a827f2b0805e250c552928ed
Lavar.Design <bjorn.osterholm@elnotorios.com>
2020-01-11 19:37:30 +0100

    Added Database Writing Service

--- VOD.Admin/TagHelpers/BtnTagHelper.cs
+++ VOD.Admin/TagHelpers/BtnTagHelper.cs
@@ -35,8 +35,56 @@ namespace VOD.Admin.TagHelpers
 
             output.TagName = "a";
 
+            var aspPageAttribute = context.AllAttributes.SingleOrDefault(p => p.Name.ToLower().Equals("asp-page"));
+            var classAttribute = context.AllAttributes.SingleOrDefault(p => p.Name.ToLower().Equals("class"));
+            var buttonstyle = btnDefault;
 
+            if (aspPageAttribute != null)
+            {
+                var pageValue = aspPageAttribute.Value.ToString().ToLower();
+                buttonstyle =
+                    pageValue.Equals("create") ? btnPrimary :
+                    pageValue.Equals("delete") ? btnDanger :
+                    pageValue.Equals("edit") ? btnSuccess :
+                    pageValue.Equals("index") ? btnPrimary :
+                    pageValue.Equals("details") ? btnInfo :
+                    pageValue.Equals("/index") ? btnWarning :
+                    pageValue.Equals("error") ? btnDanger :
+                    btnDefault;
+            }
 
+            var bootstrapClasses = $"btn-sm {buttonstyle}";
+
+            if (classAttribute != null)
+            {
+                var css = classAttribute.Value.ToString();
+
+                if (!css.ToLower().Contains("btn-"))
+                {
+                    output.Attributes.Remove(classAttribute);
+                    classAttribute = new TagHelperAttribute("class", $"{css} {bootstrapClasses}");
+                    output.Attributes.Add(classAttribute);
+                }
+            }
+            else
+            {
+                output.Attributes.Add("class", bootstrapClasses);
+            }
+
+            if (!Icon.Equals(string.Empty))
+            {
+                var childContext = output.GetChildContentAsync().Result;
+                var content = childContext.GetContent().Trim();
+                if (content.Length > 0) content = $"&nbsp{content}";
+
+                output.Content.SetHtmlContent($"<i class='{iconProvider}' style='display: inline-flex; vertical-align: top; line-height: inherit; font-size: medium;'>{Icon}</i> <span style='font-size: medium;'>{content}</span>");
+            }
+
+            var style = context.AllAttributes.SingleOrDefault(s => s.Name.ToLower().Equals("style"));
+            var styleValue = style == null ? "" : style.Value;
+            var newStyle = new TagHelperAttribute("style", $"{styleValue} display: inline-flex; border-radius: 0px; text-decoration: none;");
+            if (style != null) output.Attributes.Remove(style);
+            output.Attributes.Add(newStyle);
 
             base.Process(context, output);
         }
